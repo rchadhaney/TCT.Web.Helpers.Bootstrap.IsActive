ef58ab25ffd6eb3b7599ccf5a4bd05370d5d425f
Chad Haney <chad@tincantechnologies.com>
2016-04-27 14:10:13 -0500

    Updated helper to handle areas

--- IsactiveExample/TCT.Web.Helpers.Bootstrap/IsActiveHelpers.cs
+++ IsactiveExample/TCT.Web.Helpers.Bootstrap/IsActiveHelpers.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace TCT.Web.Helpers.Bootstrap
 {
@@ -40,36 +42,69 @@ namespace TCT.Web.Helpers.Bootstrap
             return IsActive(htmlHelper, action, controller, activeClass, "");
         }
 
-        ///// <summary>
-        ///// return active class if action is current
-        ///// </summary>
-        ///// <param name="htmlHelper"></param>
-        ///// <param name="action"></param>
-        ///// <param name="activeClass"></param>
-        ///// <param name="inActiveClass"></param>
-        ///// <returns></returns>
-        //public static MvcHtmlString IsActive(this HtmlHelper htmlHelper, string action, string activeClass, string inActiveClass = "")
-        //{
-        //    var routeData = htmlHelper.ViewContext.RouteData;
+        /// <summary>
+        /// Returns the active class when action/controller/area is true
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="action"></param>
+        /// <param name="controller"></param>
+        /// <param name="routeValues"></param>
+        /// <param name="activeClass"></param>
+        /// <param name="inActiveClass"></param>
+        /// <returns></returns>
+        public static MvcHtmlString IsActive(this HtmlHelper htmlHelper, string action, string controller, object routeValues, 
+            string activeClass, string inActiveClass = "")
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+            
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+            var areaPass = true;
 
-        //    var routeAction = routeData.Values["action"].ToString();
+            //Get routevalue properties.
+            var rvProps = routeValues.GetType().GetProperties();
+            //Get Area!
+            var area =
+                rvProps.Where(rv => rv.Name.ToLower() == "area")
+                    .Select(rv => rv.GetValue(routeValues, null))
+                    .FirstOrDefault();
+            if (area != null)
+            {
+                if (routeData.Values["area"] != null)
+                {
+                    var routeArea = routeData.Values["area"].ToString();
+                    areaPass = area.ToString().ToLower() == routeArea.ToLower();
+                }
+                else
+                {
+                    //If no area is in routedata then this is a failure!
+                    areaPass = false;
+                }
+            }
 
-        //    var returnActive = (action == routeAction);
 
-        //    return new MvcHtmlString(returnActive ? activeClass : inActiveClass);
-        //}
+            var returnActive = (controller == routeController && action == routeAction && areaPass);
 
-        ///// <summary>
-        ///// return active class if action is current
-        ///// </summary>
-        ///// <param name="htmlHelper"></param>
-        ///// <param name="action"></param>
-        ///// <param name="activeClass"></param>
-        ///// <returns></returns>
-        //public static MvcHtmlString IsActive(this HtmlHelper htmlHelper, string action, string activeClass)
-        //{
-        //    return IsActive(htmlHelper, action, activeClass);
-        //}
+            return new MvcHtmlString(returnActive ? activeClass : inActiveClass);
+        }
+
+        /// <summary>
+        /// return active class if action is current
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="action"></param>
+        /// <param name="activeClass"></param>
+        /// <returns></returns>
+        public static MvcHtmlString IsActive(this HtmlHelper htmlHelper, string action, string activeClass)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+
+            var returnActive = (action == routeAction);
+
+            return new MvcHtmlString(returnActive ? activeClass : "");
+        }
 
     }
 }
